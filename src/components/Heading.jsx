97d3196a7e2aa92d43dafe4aef0e3f21ed7086c0
NeggFriedRice <tomloo222@gmail.com>
2024-04-15 11:19:56 +1000

    Add styling to hero banner, add info to heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,6 +4,7 @@ export default function Heading() {
   return (
     <div className="py-4">
         <h1 className="font-[SwanseaBold] text-[3.2rem]">dentfix automotive<span className="text-setOrange">.</span></h1>
+        <p className="text-[1.5rem]">Mobile Paintless Dent Removal</p>
     </div>
   )
 }
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 
 export default function HeroBanner() {
 
-    const numberStyle = "bg-setOrange px-4 py-1 rounded-[50%] ml-2 mr-4"
+    const numberStyle = "bg-setOrange px-3.5 py-1.5 rounded-[50%] ml-2 mr-4 text-white"
 
     return (
-    <div className="bg-slate-900 flex justify-between ">
-        <div className="mx-4">
+    <div className="bg-slate-900 flex w-[80vw] justify-center">
+        <div className="mx-2 px-4 bg-white text-neutral-500 rounded-xl">
             <h1 className="text-[2.5rem]">Three easy steps</h1>
-            <p className="text-[2rem] my-4"><span className={numberStyle}>1</span>Send us photos for a quote</p>
-            <p className="text-[2rem] my-4"><span className={numberStyle}>2</span> Find a time that suits you</p>
-            <p className="text-[2rem] my-4"><span className={numberStyle}>3</span> We come to you!</p>
+            <div className="h-[1px] w-[80%] bg-slate-200"></div>
+            <p className="text-[1.5rem] my-10"><span className={numberStyle}>1</span>Send us photos for a quote</p>
+            <p className="text-[1.5rem] my-10"><span className={numberStyle}>2</span> Find a time that suits you</p>
+            <p className="text-[1.5rem] my-10"><span className={numberStyle}>3</span> We come to you!</p>
         </div>
-        <div className="mx-4 w-[50vw]">
+        <div className="mx-4 px-4 bg-blue-950 rounded-xl">
             <h1 className="text-[2.5rem]">Why choose us?</h1>
+            <div className="h-[1px] w-[80%] bg-slate-500"></div>
             <div className="flex place-items-center mt-4">
                 <img src="/icons/calendar.png" className="h-[35px] ml-2 mr-4" />
                 <p className="text-[1.5rem]">7 days a week</p>
             </div>
             <div className="flex place-items-center mt-3">
                 <img src="/icons/clock.svg" className="h-[35px] ml-2 mr-4" />
-                <p className="text-[1.5rem]">Same day turnaround*</p>
+                <p className="text-[1.5rem]">Same day turnaround<span className="text-setOrange">*</span></p>
             </div>
             <div className="flex place-items-center  mt-3">
                 <img src="/icons/truck.svg" className="h-[35px] ml-2 mr-4" />
@@ -32,9 +34,9 @@ export default function HeroBanner() {
             </div>
             <div className="flex place-items-center  mt-3">
                 <img src="/icons/rocket.svg" className="h-[35px] ml-2 mr-4" />
-                <p className="text-[1.5rem]">40+ years experience</p>
+                <p className="text-[1.5rem]">40+ years experience in panel beating</p>
             </div>
-            <p>*In most cases, for minor fixes</p>
+            <p className="my-2"><span className="text-setOrange">*</span> In most cases, for minor fixes</p>
         </div>
     </div>
   )
