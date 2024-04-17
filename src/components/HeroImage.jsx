09802b727444a0ccbb1668b971b46e0b12037d6b
Thomas Loo <tomloo222@gmail.com>
2024-04-17 13:01:19 +1000

    Add link to Instagram page on home component

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -20,10 +20,12 @@ export default function HeroImage() {
                     <img key="car3" src="/gallery/car-3.png" className={imageStyles}/>
                 </motion.div>
             </div>
-            <div className="flex justify-center place-items-center py-2">
-                <img src="/icons/instagram.png" className="h-[48px] hover:scale-110 transition-all duration-300 delay-100"/>
-                <p className="text-[1rem] lg:text-[1.5rem] hover:scale-105 transition-all duration-300 delay-100 px-2">Keep up to date with us on Instagram</p>
-            </div>
+            <a href="https://www.instagram.com/bodysolutionspdr/" target="_blank">
+                <div className="flex justify-center place-items-center py-2">
+                    <img src="/icons/instagram.png" className="h-[48px] hover:scale-110 transition-all duration-300 delay-100"/>
+                    <p className="text-[1rem] lg:text-[1.5rem] hover:scale-105 transition-all duration-300 delay-100 px-2">Keep up to date with us on Instagram</p>
+                </div>
+            </a>
         </>
     )
 }
