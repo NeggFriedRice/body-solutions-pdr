f79853513ca33840f04d82fbc6748600229cb391
Thomas Loo <tomloo222@gmail.com>
2024-04-18 00:59:17 +1000

    Add animated logo

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,11 +4,14 @@ import { motion } from 'framer-motion'
 export default function Heading() {
   return (
     <div className="py-4 flex justify-center">
+      <div className="relative">
+      <img src="/animated-logo.svg" className="absolute max-xs:w-[144px] w-[163px] top-[0.5px] lg:w-[480px] lg:top-[1px] lg:left-[1px]"/>
         <motion.div 
-        initial={{ opacity: 0, y: -75 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: 0.5, type: 'spring', ease: 'easeInOut'}}
+        initial={{ opacity: 0, }}
+        animate={{ opacity: 1, }}
+        transition={{ duration: 1, delay: 2.2, type: 'spring', ease: 'easeInOut'}}
         className="w-[90vw] lg:w-[1000px] flex justify-between">
+
             <Link to="/">
             <img src="/bs-logo.png" className="w-[170px] max-xs:w-[150px] lg:w-[500px]"/>
             </Link>
@@ -19,6 +22,7 @@ export default function Heading() {
                 <p className="text-[1rem] lg:text-[1.5rem] max-xs:text-[0.75rem] text-right">bodysolutionspdr@gmail.com</p>
                 </div>
         </motion.div>
+        </div>
     </div>
   )
 }
